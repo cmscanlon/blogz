1e60bd9ca718f57bc84aedc21fafcd76964985a3
Carolyn Scanlon <cmscanlon@yahoo.com>
2019-07-05 16:50:14 -0500

    Added all templates and all is working

--- main.py
+++ main.py
@@ -3,19 +3,50 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
+class Blog(db.Model):
 
-posts = []
+    id = db.Column(db.Integer, primary_key=True)
+    blog_title = db.Column(db.String(120))
+    body = db.Column(db.Text)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
+    posts = Blog.query.all()
 
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        posts.append(title)
-        posts.append(body)
+    return render_template('blog.html', title='Build A Blog', posts=posts)
 
-    return render_template('blog.html', title='Build a Blog', posts=posts)
+@app.route('/add')
+def add():
+    return render_template('add.html')    
 
-app.run()            
+@app.route('/newpost', methods=['POST'])
+def newpost():
+    blog_title = request.form['blog_title']
+    body = request.form['body']  
+
+    post = Blog(blog_title=blog_title, body=body) 
+
+    db.session.add(post)
+    db.session.commit()  
+
+    blog_title_error = ''
+    body_error = ''
+
+    if blog_title == '':
+        blog_title_error = "Please fill out title."
+    if body == '':
+        body_error = "Please fill out body content." 
+    if blog_title_error != '' or body_error != '':
+        return render_template('/add.html', blog_title=blog_title, 
+        body=body, blog_title_error=blog_title_error, 
+        body_error=body_error)           
+    else:
+        return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
+           
